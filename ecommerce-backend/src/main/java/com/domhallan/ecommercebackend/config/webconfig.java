0d20e1754a9bcef3a43d4a5109f76d00cd61c42a
Dom Hallan <dom@domhallan.com>
2024-05-23 08:51:00 -0400

    Added CORS configuration to backend
    The update introduces a Cross-Origin Resource Sharing (CORS) configuration to the backend. This allows requests from any origin and supports various HTTP methods including GET, PUT, POST, PATCH, DELETE, and OPTIONS.

--- ecommerce-backend/src/main/java/com/domhallan/ecommercebackend/config/webconfig.java
+++ ecommerce-backend/src/main/java/com/domhallan/ecommercebackend/config/webconfig.java
@@ -1,2 +1,22 @@
-package com.domhallan.ecommercebackend.config;public class webconfig {
+package com.domhallan.ecommercebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class Webconfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+      }
+    };
+  }
 }
